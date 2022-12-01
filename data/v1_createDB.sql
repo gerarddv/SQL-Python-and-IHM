e78b851c2ff00fe2a65350e70ea7f990db3663d5
DL0WKEY <gerard.deviala@gmail.com>
2022-12-01 11:52:57 +0100

    SQL features implemented

--- data/v1_createDB.sql
+++ data/v1_createDB.sql
@@ -1,5 +1,5 @@
 -- TODO 1.3a : Créer les tables manquantes et modifier celles ci-dessous
-CREATE TABLE LesSportifsEQ
+CREATE TABLE IF NOT EXISTS LesSportifs
 (
   numSp NUMBER(4),
   nomSp VARCHAR2(20),
@@ -7,13 +7,11 @@ CREATE TABLE LesSportifsEQ
   pays VARCHAR2(20),
   categorieSp VARCHAR2(10),
   dateNaisSp DATE,
-  numEq NUMBER(4),
   CONSTRAINT SP_CK1 CHECK(numSp > 0),
-  CONSTRAINT SP_CK2 CHECK(categorieSp IN ('feminin','masculin')),
-  CONSTRAINT SP_CK3 CHECK(numEq > 0)
+  CONSTRAINT SP_CK2 CHECK(categorieSp IN ('feminin','masculin'))
 );
 
-CREATE TABLE LesEpreuves
+CREATE TABLE IF NOT EXISTS LesEpreuves
 (
   numEp NUMBER(3),
   nomEp VARCHAR2(20),
@@ -29,6 +27,49 @@ CREATE TABLE LesEpreuves
   CONSTRAINT EP_CK4 CHECK (nbSportifsEp > 0)
 );
 
+CREATE TABLE IF NOT EXISTS LesEquipes
+(
+	numEq NUMBER(4) PRIMARY KEY NOT NULL,
+	nbSportifsEq NUMBER(3),
+	CONSTRAINT SP_CK3 CHECK(numEq > 0)
+);
+
+CREATE TABLE IF NOT EXISTS LesEquipiers
+(
+	numEq NUMBER(4) NOT NULL,
+	numSp NUMBER(4) NOT NULL,
+	PRIMARY KEY(numEq, numSP),
+	FOREIGN KEY(numEq) REFERENCES LesEquipes(numEq),
+	FOREIGN KEY(numSp) REFERENCES LesSportifs(numSp)
+);
+
+CREATE TABLE IF NOT EXISTS LesParticipants
+(
+	numP NUMBER(4) PRIMARY KEY,
+	numEp NUMBER(3),
+	FOREIGN KEY(numP) REFERENCES LesEquipes(numEq),
+	FOREIGN KEY(numP) REFERENCES LesSportifs(numSp)
+);
+
+CREATE TABLE IF NOT EXISTS LesResultats
+(
+	numEp NUMBER(3) PRIMARY KEY,
+	md_gold NUMBER(4),
+	md_silver NUMBER(4),
+	md_bronze NUMBER(4),
+	CONSTRAINT RE_CK1 CHECK (gold>0),
+	CONSTRAINT RE_CK2 CHECK (silver>0),
+	CONSTRAINT RE_CK3 CHECK (bronze> 0)
+);
+
 -- TODO 1.4a : ajouter la définition de la vue LesAgesSportifs
+CREATE VIEW IF NOT EXISTS LesAgesSportifs AS
+	SELECT numSp, nomSp, prenomSp, pays, categorieSp, dateNaisSp, DATEDIFF(year, dateNaisSp, CURRENT_DATE)
+	FROM LesSportifs;
+
 -- TODO 1.5a : ajouter la définition de la vue LesNbsEquipiers
+CREATE VIEW IF NOT EXISTS LesNbsEquipiers AS
+	SELECT numEq, COUNT(numSp)
+	FROM LesEquipiers
+	GROUP BY numEq;
 -- TODO 3.3 : ajouter les éléments nécessaires pour créer le trigger (attention, syntaxe SQLite différent qu'Oracle)
--- data/v1_deleteDB.sql
+++ data/v1_deleteDB.sql
@@ -1,4 +1,9 @@
 -- TODO 1.3a : Détruire les tables manquantes et modifier celles ci-dessous
 DROP TABLE IF EXISTS LesEpreuves;
-DROP TABLE IF EXISTS LesSportifsEQ;
+DROP TABLE IF EXISTS LesSportifs;
+DROP TABLE IF EXISTS LesParticipants;
+DROP TABLE IF EXISTS LesEquipes;
+DROP TABLE IF EXISTS LesEquipiers;
+DROP TABLE IF EXISTS LesParticipants;
+DROP TABLE IF EXISTS LesResultats;
 -- TODO 3.3 : pensez à détruire vos triggers !
